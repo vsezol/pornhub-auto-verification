ca788ae654180466e58c882648ad9cdf1601d9f4
vsezol <50067638+vsezol@users.noreply.github.com>
2020-05-04 13:25:06 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+const containerId = '#age-verification-container';
+const wrapperId = '#age-verification-wrapper';
+document.querySelector(containerId).remove();
+document.querySelector(wrapperId).remove();
